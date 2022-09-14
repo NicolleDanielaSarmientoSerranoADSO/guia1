6a1997974afcfe292bf30b34dfa6f613859b1967
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-09-14 10:18:20 -0500

    ejercicio13

--- main12.js
+++
@@ -1,8 +0,0 @@
-addEventListener("DOMContentLoaded",()=>{
-    let lla=int(input("coloque el valor de su llamada:"))
-    let min1=355
-    let iva=0.20
-    let iv=(lla*min1)*iva
-    let man=iv+(lla*min1)
-    console.log(`el valor de la llamada es: ${man}`);
-})
