f710da5b1cd32333dd203b120bfa98a39b395c0e
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-09-14 10:19:25 -0500

    ejercicio15

--- main14.js
+++
@@ -1,7 +0,0 @@
-addEventListener("DOMContentLoaded",()=>{
-    let p=Number(prompt(`coloque aqui el valor del producto `))
-    let d=Number (0.05)
-    let res=Number(p*d)
-    let des=Number(p-res)
-    console.log(`el precio final mas el descuent es:${des}`);
-})
---
+++ main15.js
@@ -0,0 +1,5 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    let c=Number(prompt(`ingrese los grados celcius:`))
+    let f= 9/5*c+32
+    console.log(`en grados Fahrenheit:°${f}`);
+})
