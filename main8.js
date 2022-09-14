a95fdad4a6650d36df0afa2a4ba50488b7f34926
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-09-14 10:14:28 -0500

    ejercicio9

--- main8.js
+++
@@ -1,9 +0,0 @@
-addEventListener("DOMContentLoaded",()=>{
-    let a=0,b=0,c=0
-    a=Number(prompt("Ingrese el primer valor: ",4))
-    b=Number(prompt("Ingrese el segundo valor:",2))
-    c=Number(prompt("Ingrese el tercer valor:",5))
-    let exp1=0,exp2=0,exp3=0
-    exp1=(a+7*c)/((b+2-a)+2*b)
-    console.log(`el valor de la primera expresión es: ${exp1}`)
-})
